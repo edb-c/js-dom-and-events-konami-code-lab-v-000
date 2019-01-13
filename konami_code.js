076e1d7eabf3ad03dda60689435ddbe139c38faa
edb-c <edb.cofresi@gmail.com>
2019-01-13 12:35:13 -0500

    clean code

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+ // Write your JavaScript code inside the init() function
 
 function init() {
+ 
+  const body = document.querySelector('body');
+  
   let index = 0;
 
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
+  body.addEventListener('keydown', function onKeyDownHandler(e) {
+  
+    const key = parseInt(e.which || e.location);
 
-    if (code[index] === key) {
-      index++;
+    if (key === code[index]) {
+      index++
 
       if (index === code.length) {
-        alert('Hurray!');
+        alert('Congratulations! Konami Code accepted.');
 
         index = 0;
       }
@@ -20,4 +26,4 @@ function init() {
   });
 }
 
-init();
+init()
